e6c877811ed4cb3b8b593e409043ed686b4f0a22
swagner-101 <wagners113@hotmail.com>
2019-01-04 23:33:31 -0800

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Scott Wagner on 2019-01-04.
+//  Copyright © 2019 Scott Wagner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
